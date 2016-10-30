7fa8fd0bbf479aa9371be95caf0094b009b97062
napster113 <napster1376@gmail.com>
2016-10-30 18:03:51 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -78,150 +78,144 @@ REPLACE="
 
 # Construct your own list
 REPLACE="
-  /system/app/WebManual
-	/system/app/Drive
-	/system/app/Dropbox_zero
-	/system/app/Hangouts
-	/system/app/Maps
-	/system/app/Music2
-	/system/app/PlayGames
-	/system/app/YouTube
-	
-	/system/app/WhatsAppDownloader
-  /system/app/FlipboardBriefing
-  /system/app/SBrowser_4_LATEST
-	/system/app/OneDrive_Samsung
-	/system/app/MyGalaxyStub
-	/system/app/Books
-	/system/app/ChromeCustomizations
-	/system/app/Facebook_stub
-	/system/app/FBInstagram_stub
-	/system/app/FBMessenger_stub
-	/system/app/FBAppManager
-	/system/app/Gmail2
-	/system/app/MSSkype_stu	
-	/system/app/Newsstand
-	/system/app/Sidesync
-	/system/app/OneDrive
-	/system/app/OneNote	
-	/system/app/PlusOne
-	/system/app/Photos
-	/system/app/SmartRemote_zero
-	/system/app/Videos
-	/system/app/MobilePrintSvc_Samsung	
-  /system/app/ColorBlind_M
-  /system/app/ChocoEUKor	
-  /system/app/AdvSoundDetector2015
-	/system/app/AssistantMenu_M
-	/system/app/BeamService
-	
-	/system/priv-app/DeezerTROffer
-	/system/priv-app/SETKCustomizationV2_1
-	/system/priv-app/Excel_SamsungStub
-	/system/priv-app/PowerPoint_SamsungStub
-	/system/priv-app/Word_SamsungStub
-	/system/priv-app/HancomOfficeViewer
-	/system/priv-app/WhatsAppDownloader
-	/system/priv-app/SamsungPayStub
-	/system/priv-app/GalaxyCare
-	/system/priv-app/GearManager
-  /system/priv-app/KLMSAgent
-	/system/priv-app/VoiceWakeUp
-	/system/priv-app/VoiceNote_4.0
-
-  /system/app/Bridge
-	/system/app/BBCAgent
-	/system/app/ELMAgent
-	/system/app/MyKNOXSetupWizard
-	/system/app/SamsungDLPService
-	/system/app/SysScope
-	/system/app/SecurityLogAgent
-	/system/app/ContainerAgent
-	/system/app/ContainerEventsRelayManager
-	/system/app/kioskdefault
-	/system/app/KLMSAgent
-	/system/app/Knox
-	/system/app/KNOX
-	/system/app/KnoxAppsUpdateAgent
-  /system/app/KnoxAttestationAgent
-  /system/app/KnoxFolderContainer
-  /system/app/KnoxSetupWizardClient
-  /system/app/KnoxSwitcher
-	/system/app/RCPComponents
-	/system/app/SwitchKnoxI
-	/system/app/UniversalMDMClient
-	/system/priv-app/FotaAgent
-	/system/priv-app/SPDClient
-	/system/priv-app/UrgentFWUpdate
-	/system/priv-app/UrgentFWUpdateNfc
-	/system/container
-	/system/preloadedkiosk
-	/system/preloadedsso
-	/system/preloadedmdm
-	/system/etc/secure_storage/com.sec.knox
-	/system/etc/secure_storage/com.samsung.android.securitylogagent
-
-  /system/app/AntHalService
-	/system/app/ANTPlusPlugins
-	/system/app/ANTPlusTest
-	/system/app/ANTRadioService
-	/system/app/GearFit
-	/system/app/GearFitInstaller
-	/system/app/GearManagerStub
-	/system/priv-app/GearManager
-	/system/priv-app/RNB
-	/system/priv-app/RNBShell
-	/system/priv-app/VRSetupWizardStub
-
-  /system/app/SapaAudioConnectionService
-	/system/app/SapaMonitor
-	/system/priv-app/Hearingdro_V5
-
-  /system/app/BasicDreams
-	/system/app/PhotoTable
-	/system/priv-app/SecLiveWallpapersPicker
-
-  /system/app/SamsungTTS
-	/system/app/WeatherProvider
-	/system/tts
-	/system/voicebargeindata
-	/system/wakeupdata
-	/system/priv-app/SVoiceLang_EnglishPack_GB_1.0
-  /system/priv-app/SVoiceLang_EnglishPack_US_1.0
-  /system/priv-app/SVoiceLang_FrenchPack_1.0
-  /system/priv-app/SVoiceLang_GermanPack_DE_1.0
-  /system/priv-app/SVoiceLang_ItalianPack_IT_1.0
-  /system/priv-app/SVoiceLang_RussianPack_RU_1.0
-  /system/priv-app/SVoiceLang_SpanishPack_ES_1.0
-	/system/priv-app/SVoice
-	/system/priv-app/SVoiceLang
-	/system/priv-app/SVoice_1.0
-	/system/priv-app/VoiceWakeUp
-	/system/priv-app/SVoiceIME
-	/system/priv-app/SVoicePLM
-	/system/priv-app/SVoicePLM_1.0
-	/system/priv-app/SVoiceService
-
-  /system/app/CarmodeStub
-	/system/app/Carmode_Del
-	
-	/system/app/AllShareCastPlayer
-	/system/app/AllshareFileShare
-	/system/app/AllshareFileShareClient
-	/system/app/AllshareFileShareServer
-	/system/app/AllshareMediaShare
-	/system/app/WfdBroker
-	/system/lib/libasf_fileshare.so
-	/system/lib/libasf_fileshareserver.so
-	/system/lib/libasf_mediashare.so
-	
-	/system/priv-app/SecEmail_M
-	/system/lib/libkerberos.so
-	/system/lib64/libkerberos.so
-	
-	/system/app/GoogleTTS
-	/system/priv-app/Velvet
-	/system/usr/srec
+/system/app/WebManual
+/system/app/Drive
+/system/app/Dropbox_zero
+/system/app/Hangouts
+/system/app/Maps
+/system/app/Music2
+/system/app/PlayGames
+/system/app/YouTube	
+/system/app/WhatsAppDownloader
+/system/app/FlipboardBriefing
+/system/app/SBrowser_4_LATEST
+/system/app/OneDrive_Samsung
+/system/app/MyGalaxyStub
+/system/app/Books
+/system/app/ChromeCustomizations
+/system/app/Facebook_stub
+/system/app/FBInstagram_stub
+/system/app/FBMessenger_stub
+/system/app/FBAppManager
+/system/app/Gmail2
+/system/app/MSSkype_stu	
+/system/app/Newsstand
+/system/app/Sidesync
+/system/app/OneDrive
+/system/app/OneNote	
+/system/app/PlusOne
+/system/app/Photos
+/system/app/SmartRemote_zero
+/system/app/Videos
+/system/app/MobilePrintSvc_Samsung	
+/system/app/ColorBlind_M
+/system/app/ChocoEUKor	
+/system/app/AdvSoundDetector2015
+/system/app/AssistantMenu_M
+/system/app/BeamService	
+/system/priv-app/DeezerTROffer
+/system/priv-app/SETKCustomizationV2_1
+/system/priv-app/Excel_SamsungStub
+/system/priv-app/PowerPoint_SamsungStub
+/system/priv-app/Word_SamsungStub
+/system/priv-app/HancomOfficeViewer
+/system/priv-app/WhatsAppDownloader
+/system/priv-app/SamsungPayStub
+/system/priv-app/GalaxyCare
+/system/priv-app/GearManager
+/system/priv-app/KLMSAgent
+/system/priv-app/VoiceWakeUp
+/system/priv-app/VoiceNote_4.0
+/system/app/Bridge
+/system/app/BBCAgent
+/system/app/ELMAgent
+/system/app/MyKNOXSetupWizard
+/system/app/SamsungDLPService
+/system/app/SysScope
+/system/app/SecurityLogAgent
+/system/app/ContainerAgent
+/system/app/ContainerEventsRelayManager
+/system/app/kioskdefault
+/system/app/KLMSAgent
+/system/app/Knox
+/system/app/KNOX
+/system/app/KnoxAppsUpdateAgent
+/system/app/KnoxAttestationAgent
+/system/app/KnoxFolderContainer
+/system/app/KnoxSetupWizardClient
+/system/app/KnoxSwitcher
+/system/app/RCPComponents
+/system/app/SwitchKnoxI
+/system/app/UniversalMDMClient
+/system/priv-app/FotaAgent
+/system/priv-app/SPDClient
+/system/priv-app/UrgentFWUpdate
+/system/priv-app/UrgentFWUpdateNfc
+/system/container
+/system/preloadedkiosk
+/system/preloadedsso
+/system/preloadedmdm
+/system/etc/secure_storage/com.sec.knox
+/system/etc/secure_storage/com.samsung.android.securitylogagent
+/system/app/AntHalService
+/system/app/ANTPlusPlugins
+/system/app/ANTPlusTest
+/system/app/ANTRadioService
+/system/app/GearFit
+/system/app/GearFitInstaller
+/system/app/GearManagerStub
+/system/priv-app/GearManager
+/system/priv-app/RNB
+/system/priv-app/RNBShell
+/system/priv-app/VRSetupWizardStub
+/system/app/SapaAudioConnectionService
+/system/app/SapaMonitor
+/system/priv-app/Hearingdro_V5
+/system/app/BasicDreams
+/system/app/PhotoTable
+/system/priv-app/SecLiveWallpapersPicker
+/system/app/SamsungTTS
+/system/app/WeatherProvider
+/system/tts
+/system/voicebargeindata
+/system/wakeupdata
+/system/priv-app/SVoiceLang_EnglishPack_GB_1.0
+/system/priv-app/SVoiceLang_EnglishPack_US_1.0
+/system/priv-app/SVoiceLang_FrenchPack_1.0
+/system/priv-app/SVoiceLang_GermanPack_DE_1.0
+/system/priv-app/SVoiceLang_ItalianPack_IT_1.0
+/system/priv-app/SVoiceLang_RussianPack_RU_1.0
+/system/priv-app/SVoiceLang_SpanishPack_ES_1.0
+/system/priv-app/SVoice
+/system/priv-app/SVoiceLang
+/system/priv-app/SVoice_1.0
+/system/priv-app/VoiceWakeUp
+/system/priv-app/SVoiceIME
+/system/priv-app/SVoicePLM
+/system/priv-app/SVoicePLM_1.0
+/system/priv-app/SVoiceService
+/system/app/CarmodeStub
+/system/app/Carmode_Del
+/system/app/AllShareCastPlayer
+/system/app/AllshareFileShare
+/system/app/AllshareFileShareClient
+/system/app/AllshareFileShareServer
+/system/app/AllshareMediaShare
+/system/app/WfdBroker
+/system/lib/libasf_fileshare.so
+/system/lib/libasf_fileshareserver.so
+/system/lib/libasf_mediashare.so
+/system/priv-app/SecEmail_M
+/system/lib/libkerberos.so
+/system/lib64/libkerberos.so
+/system/app/GoogleTTS
+/system/priv-app/Velvet
+/system/usr/srec
+/system/app/CoolEUKor
+/system/app/GalaxyAppsWidget_Phone
+/system/app/HealthService
+/system/app/mygalaxy_app-release-SDK_23_150116
+/system/priv-app/SecMyFiles
 "
 
 ##########################################################################################
